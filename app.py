7a2083e169eb2cefe361e643e15d1690af81fa8a
tongytg0 <142465756+tongytg0@users.noreply.github.com>
2024-07-04 21:33:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
-#import lib
 from flask import Flask, render_template, request
+from IPython.display import display, HTML
 
-#api google
-#api google
+# API Google
 import google.generativeai as palm
-import os
-api_key = os.getenv("API_TOKEN")
-palm.configure(api_key=api_key)
+palm.configure(api_key="AIzaSyBF-fXqYk5VLQ6E7UIuiViLxQ-B9wScdPI")
 model = {"model": "models/chat-bison-001"}
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return(render_template("index.html"))
+    return render_template("index.html")
 
-@app.route("/main", methods=["GET","POST"])
+@app.route("/main", methods=["GET", "POST"])
 def main():
-    return(render_template("main.html"))
+    return render_template("main.html")
 
-@app.route("/result", methods=["GET","POST"])
+@app.route("/result", methods=["GET", "POST"])
 def result():
     a = request.form.get("wd")
     s = request.form.get("pos")
@@ -28,11 +25,11 @@ def result():
     g = request.form.get("sex")
     h = request.form.get("exp")
     j = request.form.get("sal")
-    resultsult = a,"company is recruiting",s,"will",d,"degree with",f,"age and",g,"gender with",h,"experience and",j,"this requested salary will this person make my company better give me the answer in the HR team prospective. No more than 50 words."
+    resultsult = a, "company is recruiting", s, "will", d, "degree with", f, "age and", g, "gender with", h, "experience and", j, "this requested salary will this person make my company better give me the answer in the HR team perspective. No more than 50 words."
     print(resultsult)
     print(a)
     resultdone = palm.chat(**model, messages=resultsult)
-    return(render_template("result.html",resultdone=resultdone.last))
+    return render_template("result.html", resultdone=resultdone.last)
 
 if __name__ == "__main__":
     app.run()
